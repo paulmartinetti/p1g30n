6e57fc1516ebac765776421dbd4cf57e8859e789
Paul Martinetti <airhead@Pauls-MacBook-Air.local>
2020-02-16 07:33:43 +0100

    all start up files setup

---
+++ game.js
@@ -0,0 +1,77 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 1200,
+    physics: {
+        default: 'matter',
+        matter: {
+            debug: true
+        }
+    },
+    scene: {
+        preload: preload,
+        init: init,
+        create: create,
+        update: update
+    },
+    title: 'p1g30n',
+    pixelArt: false,
+    backgroundColor: '555555'
+};
+
+// global vars
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+    // pigeon
+    this.load.atlas('pigeon', 'assets/spritesheet.png', 'assets/sprites.json');
+}
+
+function init() {
+    this.gameW = this.sys.game.config.width;
+    this.gameH = this.sys.game.config.height;
+
+    this.cursors;
+}
+
+function create() {
+
+    console.log(this.anims.create({
+        key: 'agauche',
+        frames: this.anims.generateFrameNames('pigeon',{
+                prefix: 'body_',
+                start: 0,
+                end: 15,
+                zeroPad: 1
+            }),
+        repeat: -1
+    }));
+    this.p = this.add.sprite(400,400,'pigeon');
+    //this.p.msPerFrame = 125;
+    this.p.play('agauche');
+
+
+    // physics - matter
+    this.matter.world.setBounds(0, 0, 1200, 1200);
+    //let wall = this.matter.add.sprite(0, 1100, 'wall');
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (this.cursors.up.isDown) {
+
+    } else {
+        //
+    }
+    // turning
+    if (this.cursors.left.isDown) {
+
+    } else if (this.cursors.right.isDown) {
+
+    } else {
+
+    }
+}
